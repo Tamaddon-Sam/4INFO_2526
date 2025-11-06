2b297a4c4c43a219e5e7fb61e3f1642b1c1b89b2
tamaddon-sam <sam.tamaddon08@gmail.com>
2025-11-06 15:20:56 +0100

    es

---
+++ TPSI/tamaddon_sam/javascript/tamaddon_esercizio_js_09.js
@@ -0,0 +1,7 @@
+function login(){
+    let username = prompt("Inserisci il nome dell'utente: "); 
+    username == null || username == `Admin` || alert(`non so chi tu sia inserisci username giusto`);
+    username == "Admin" && (password = prompt(`Password`));
+    password == null || password == "Agnell1ino" || allert(`password sbagliata`);
+    password == "Agnell1ino" && alert(`benvenuto`);
+}
