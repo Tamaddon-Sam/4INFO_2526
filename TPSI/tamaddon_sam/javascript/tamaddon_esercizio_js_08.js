766ed95252365f773377c2fe300df47580b6e157
tamaddon-sam <sam.tamaddon08@gmail.com>
2025-11-24 08:21:53 +0100

    es

--- TPSI/tamaddon_sam/javascript/tamaddon_esercizio_js_08.js
+++ TPSI/tamaddon_sam/javascript/tamaddon_esercizio_js_08.js
@@ -39,9 +39,10 @@ function somma(){
   let tabella = document.getElementById("paghe")
   for (let i = 2; i < tabella.children.length; i++) {
     riga = tabella.children[i]
-    cella = Number (riga.children[4])
+    cella = Number (riga.children[4].innerText)
     somma = cella + somma
   }
+
   let body = document.getElementById("body")
   let tabellaSomma = document.createElement("table")
   let rigaLineaSup = document.createElement("tr")
@@ -54,7 +55,6 @@ function somma(){
   let hrRigaSup = document.createElement("hr")
   let hrRigaInf = document.createElement("hr")
   
-  tdCentrPrezzo.id = "sommaTdValPrezzo"
 
   tdRigaSup.colSpan = 5
   tdCentrTotale.colSpan = 4
@@ -76,10 +76,52 @@ function somma(){
   tdRigaInf.append(hrRigaInf)
   body.append(tabellaSomma)
 
+
+
+
+}
+function media(){
+  let somma = 0
+  
+  let tabella = document.getElementById("paghe")
+  for (let i = 2; i < tabella.children.length; i++) {
+    riga = tabella.children[i]
+    cella = Number (riga.children[4].innerText)
+    somma = cella + somma
+  }
+  let media = somma /cella.length
+
+  let body = document.getElementById("body")
+  let tabellaSomma = document.createElement("table")
+  let rigaLineaSup = document.createElement("tr")
+  let rigaPrezzo = document.createElement("tr")
+  let rigaLineaInf = document.createElement("tr")
+  let tdRigaSup = document.createElement("td")
+  let tdCentrTotale = document.createElement("td")
+  let tdCentrPrezzo = document.createElement("td")
+  let tdRigaInf = document.createElement("td")
+  let hrRigaSup = document.createElement("hr")
+  let hrRigaInf = document.createElement("hr")
   
 
+  tdRigaSup.colSpan = 5
+  tdCentrTotale.colSpan = 4
+  tdRigaInf.colSpan = 5
+  tdCentrPrezzo.classList.add("importo")
+  
 
+  tdCentrTotale.innerHTML = "TOTALE"
+  tdCentrPrezzo.innerHTML = `${media} €`
 
-
+  tabellaSomma.append(rigaLineaSup)
+  tabellaSomma.append(rigaPrezzo)
+  tabellaSomma.append(rigaLineaInf)
+  rigaLineaSup.append(tdRigaSup)
+  rigaPrezzo.append(tdCentrTotale)
+  rigaPrezzo.append(tdCentrPrezzo)
+  rigaLineaInf.append(tdRigaInf)
+  tdRigaSup.append(hrRigaSup)
+  tdRigaInf.append(hrRigaInf)
+  body.append(tabellaSomma)
 }
 
